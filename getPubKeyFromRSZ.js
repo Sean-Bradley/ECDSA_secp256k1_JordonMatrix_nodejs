7ea58c21b9beccdb780119bd6f816833191c7b07
seanwasere youtube <seanwasere@gmail.com>
2017-11-14 18:55:17 +0000

    Update getPubKeyFromRSZ.js

--- getPubKeyFromRSZ.js
+++ getPubKeyFromRSZ.js
@@ -97,41 +97,112 @@ var add = function (a, b) {
 }
 
 var sub = function (a, b) {
-    return from_jordan(jordan_add(to_jordan(a), to_jordan([b[0], b[1].multiply(-1)])))
+    return from_jordan(jordan_add(to_jordan(a), to_jordan([b[0], b[1].multiply(MINUSONE).add(P).mod(P)])))
 }
 
 var negate = function (a) {
     return [a[0], P.subtract(a[1]).mod(P)];
 }
 
-//¹²³⁴⁵⁶⁷⁸⁹⁰
-var N = new BN('fffffffffffffffffffffffffffffffebaaedce6af48a03bbfd25e8cd0364141', 16);
+var ecPoint = function(a) {
+    return mul([X,Y], a);
+}
+
+
+
+//How to extract public key using the R, S and Z values of a bitcoin transaction input.
+
+//secp256k1 constants
 var P = new BN('fffffffffffffffffffffffffffffffffffffffffffffffffffffffefffffc2f', 16);  //2²⁵⁶ - 2³² - 2⁹ - 2⁸ - 2⁷ - 2⁶ - 2⁴ - 1
-
-
-
-var Pp1d4 = new BN('3fffffffffffffffffffffffffffffffffffffffffffffffffffffffbfffff0c', 16);
+var N = new BN('fffffffffffffffffffffffffffffffebaaedce6af48a03bbfd25e8cd0364141', 16);
 var ZERO = new BN(0);
-var ONE = new BN(1);
-var TWO = new BN(2);
-var THREE = new BN(3);
 var SEVEN = new BN(7);
 var X = new BN('79be667ef9dcbbac55a06295ce870b07029bfcdb2dce28d959f2815b16f81798', 16);
 var Y = new BN('483ada7726a3c4655da4fbfc0e1108a8fd17b448a68554199c47d08ffb10d4b8', 16);
 
+//other constants my functions use
+var ONE = new BN(1);
+var MINUSONE = new BN(-1);
+var TWO = new BN(2);
+var THREE = new BN(3);
+var Pp1d4 = new BN('3fffffffffffffffffffffffffffffffffffffffffffffffffffffffbfffff0c', 16);
 
-//now recover public key
-//http://2coin.org/index.html?txid=5cc5364a43ee9212387bfd45fa2e2c5e8ed7b2a32fa2f8e3084f6c8845ca3e15
-var R = new BN('3b78ce563f89a0ed9414f5aa28ad0d96d6795f9c63', 16);
-var S = new BN('5d38a230719d7282d6650c7a19ea810531501d391448aa61061828a073da404d', 16);
-var Z = new BN('422e18e3c79ed7ba9eb41754d26ab846d6c0644abb51eeca74a8973d4d8e656b', 16);
+
+//Intro, there are two types of numbers in use in this demo,
+//1 - Integers, more specifically big numbers, and they are all modded N
+//2 - ecPoint, a point on 2 Dimensional graph. More specifically, the points produced by the secp256k1 Elliptic Curve equation y² = x³ + 7
+
+//Lets get some R, S and Z values.
 
 //http://2coin.org/index.html?txid=9ec4bc49e828d924af1d1029cacf709431abbde46d59554b62bc270e3b29c4b1
-//var R = new BN('d47ce4c025c35ec440bc81d99834a624875161a26bf56ef7fdc0f5d52f843ad1', 16);
-//var S = new BN('44e1ff2dfd8102cf7a47c21d5c9fd5701610d04953c6836596b4fe9dd2f53e3e', 16);
-//var Z = new BN('c0e2d0a89a348de88fda08211c70d1d7e52ccef2eb9459911bf977d587784c6e', 16);
+var R = new BN('d47ce4c025c35ec440bc81d99834a624875161a26bf56ef7fdc0f5d52f843ad1', 16);
+var S = new BN('44e1ff2dfd8102cf7a47c21d5c9fd5701610d04953c6836596b4fe9dd2f53e3e', 16);
+var Z = new BN('c0e2d0a89a348de88fda08211c70d1d7e52ccef2eb9459911bf977d587784c6e', 16);
 
-//y² = x³ + 7    <--- secp256k1 curve
+//http://2coin.org/index.html?txid=5cc5364a43ee9212387bfd45fa2e2c5e8ed7b2a32fa2f8e3084f6c8845ca3e15
+//var R = new BN('3b78ce563f89a0ed9414f5aa28ad0d96d6795f9c63', 16);
+//var S = new BN('5d38a230719d7282d6650c7a19ea810531501d391448aa61061828a073da404d', 16);
+//var Z = new BN('422e18e3c79ed7ba9eb41754d26ab846d6c0644abb51eeca74a8973d4d8e656b', 16);
+
+//http://2coin.org/index.html?txid=9312ccafb8aa624afe7fb7b4201a0ccc2a14ca2b8b8a3253093b975a6a85a280
+//var R = new BN('262e481b6d8905b5adba67aff05eb8261501b0a9434c0b7f043d00cf8d23c91b', 16);
+//var S = new BN('00bf82c0d212f30d3a0599e9b879516d762eaf5688ab83787cf470e99af5a69171', 16);
+//var Z = new BN('e2b8acb01c0ea6a2a1273fc9dbbe3cdd58c68afb54e240e1f51abcc652468204', 16);
+
+//http://2coin.org/index.html?txid=a963c57ba8a384bf708d5cf83c932e9174ebd0f82f3820e25dcc8a3d508aed54
+//var R = new BN('56cdf5abd9230a287712d7870f36e1e4005c41c99e008791364cce756b7b64b3', 16);
+//var S = new BN('00b443edc4471c5d1a037a1286019b886c0aa48675756db7f06adfb6f1c2a88f7e', 16);
+//var Z = new BN('4714f14e24a9beab8af71158e9e4a2609b45abe19152e2d674b7674913ab8692', 16);
+
+//http://2coin.org/index.html?txid=19d66411a5aa716a04b37197c11c93c9446a54694a2d2302093d8b0a93ed5d83
+//var R = new BN('00cabc3692f1f7ba75a8572dc5d270b35bcc00650534f6e5ecd6338e55355454d5', 16);
+//var S = new BN('0437b68b1ea23546f6f712fd6a7e5370cfc2e658a8f0245628afd8b6999d9da6', 16);
+//var Z = new BN('109a80161c75f67ff6b98166b061e82e4b739ee8cac2820f173ab8b1f9991242', 16);
+
+//http://2coin.org/index.html?txid=b4e7193c972ababd82b5feefe1af96b33bf062d2b959f44816a532e05940d032
+//var R = new BN('262e481b6d8905b5adba67aff05eb8261501b0a9434c0b7f043d00cf8d23c91b', 16);
+//var S = new BN('00d45c9ef85bb65a2a5bb3cf862124188adfe5fb8d430bc4b0d1222b1704d10d19', 16);
+//var Z = new BN('66042ab1a1befe137de5328ff1d4b263604824fe33c739d3a80565afdd94b34d', 16);
+
+//http://2coin.org/index.html?txid=bf474b96908ba7769120b2e8f2bfcbd2deca80c99b576b4b63bf18fb69e3d242
+//var R = new BN('35e4dd6e4d56638eee57fddf6af2d9f8a1cd8ae35d8e304b175f8c5ec0d80f6f', 16);
+//var S = new BN('24e0255335dc10284b3df9feadd1edc2bfb0540c03d1dbc09d65a84179f3b3a7', 16);
+//var Z = new BN('75c991a64fa69368d1988c2ad6f885b17b8a69868763852b2c86aacc6b4745e8', 16);
+
+//http://2coin.org/index.html?txid=85407f7fa84f5a4484677ca93c007ba4b65851727e9de8250dbf641e54e3c64f
+//var R = new BN('4132afc756fb11e23f782ecd5bf35808de865f292876ce97873748ccfac3fc86', 16);
+//var S = new BN('19f5ad1b23e3c98bafe6fe6f6cdfa38ec91e932aa56fc31183e89c1f1304ef8d', 16);
+//var Z = new BN('a13067843f92abe54f020e3bf51333fa45127c28bdce70c3c22714c7c22e5c13', 16);
+
+//http://2coin.org/index.html?txid=8e35c79125013bf900d976cf6247e311c9ca7766ec08309401f8da342c024122
+//var R = new BN('00ff1deb28d750b76b7dfde8c7ad94ce093f6b510c18ec7befa6070afe8be63852', 16);
+//var S = new BN('50cff9afaccf63b886e9fe31add54160f41854aa1776b4cbadbfa733084cfe85', 16);
+//var Z = new BN('72907d7e161ef2ca527ca381fa7e635e99faf2846582a28921428051d158365e', 16);
+
+//http://2coin.org/index.html?txid=fea40dbef8dc5479591f15e7022ee3047d768c97cd0e8fcbefc015eebcabffe1
+//var R = new BN('00c2cfb509ecda920fa8e509fb6772fc0e69d123c1efc789e65c8e0a7c98b39d20', 16);
+//var S = new BN('4ade22e432535ac5ac671658b9a64e0f81d11a32754be65001236674b0bf6794', 16);
+//var Z = new BN('437a504713ff63154b0ee63f07ff0cc988239cc73061c8a8f93078d23064441d', 16);
+
+//http://2coin.org/index.html?txid=97f136d0104b150abfe379e86c0441be4460262d69161483fdbc2cf70a2615ce
+//var R = new BN('21adb82a4cec5007cc494047c3f2c6a5dda8de342fe68020d256d5c185ccbb3a', 16);
+//var S = new BN('00fdfa125741412278a00a6bad077aed5b01db2a33dbe90323ab52eda8f9f83481', 16);
+//var Z = new BN('e34c2b45c02b4f89d03109da1cf4d2a3be0a8b8a5a413f3bcaf353aa55369d28', 16);
+
+//http://2coin.org/index.html?txid=535b898508430cf87947f69adec9dafee7250e720d2e839c7870f1d1fe1b2eda
+//var R = new BN('00f6968a978790ab2926eaca3dbd5b4b1c174ced093dbc0fdd4bf3b12028dc8dee', 16);
+//var S = new BN('45713db4c27106adb74cfdb47fcf5e2dc82faf62caf766766ad36cf8e948a7e3', 16);
+//var Z = new BN('1337c9f42cd25b003ba15d85b7ad26aa849e9e35327b74a1fda01ed43b893db6', 16);
+
+//http://2coin.org/index.html?txid=2588f2a1675c951a86c34691be0ed9cc0bb4c3e34a43828873333f0e5c515729
+//var R = new BN('44fd13a6632ffc105ff34eab7bb451c0b41c2e4893cbe34ca083ba063c70a523', 16);
+//var S = new BN('46908f86d324177264a86d2616c2dddfa4772819efe5766312bd67d8dcf54563', 16);
+//var Z = new BN('1ce2680a4ef7be67649206c8cb70f9e502053bb669d0c4fd4f7305917b83ac3f', 16);
+
+//http://2coin.org/index.html?txid=15a6c7e04626ab424175e74ea7bbc2e2f26faf5c7ec3da5b37b764265e1e32ce
+//var R = new BN('7d17d3683ec47bece3de5a691627ee9cf4b26fafcc7972edce6af1541a463b7f', 16);
+//var S = new BN('1fdf354d5334cf30fa20b5c7187f1ba74ed2da1cbb5914cc469d960723e99a6b', 16);
+//var Z = new BN('e50150f4644c9fd35937e839e0a77c2e69cbdb955c313a3ffa88be4342fa5621', 16);
 
 console.log("R                = " + R.toString(16));
 console.log("S                = " + S.toString(16));
@@ -139,28 +210,56 @@ console.log("Z                = " + Z.toString(16));
 
 console.log();
 
-//R is the x coordinate of an ecPoint where the x value = R, so we need to find the y value.
-//we can use the function y² = x³ + 7
+
+
+//Now for some background info,
+//To recover a private key, the function is,
+//privKey = (((S * K) - Z) / R) % N      <--- Integer (mod N) equation to find the private key.
+//privKey = (((S * K) / R) - (Z / R)) % N
+//to get the public key, you can use the elliptic curve equivalent of this function. 
+//pubKey  = ecPoint((((S * K) / R) - (Z / R)) % N)
+//pubKey  = ecPoint((((S * K) / R) % N) - ecPoint((Z / R)) % N))
+//pubKey  = ecPoint((S * K) / R) - ecPoint(Z / R))
+//pubKey  = ecPoint((K * S) / R) - ecPoint(Z / R))
+//pubKey  = ecPoint(K * (S / R)) - ecPoint(Z / R))
+//pubKey  = (ecPoint(K) * (S / R)) - ecPoint(Z / R))
+
+//R = ecPoint(K).x coordinate;
+//R is the x coordinate of an ecPoint whose x value = R. We need to find the y value that produces this x value.
+//We can use this function,
+//y² = x³ + 7    <----this is the equation of the secp256k1 curve 
+
+
+
+
 var x = R;
 console.log("x                = " + x.toString(16));
 
 var ySquared = x.pow(THREE).add(SEVEN).mod(P);
 console.log("y²               = " + ySquared.toString(16));
+//there are two possible answers that produce a squared result, eg, 4 = 2² or -2². We only need one of them, doesn't matter which.
 
-//there are two possible answers that produce a squared result, ie, 4 = 2² or -2². We only need one of them, doesn't matter which.
 var y = ySquared.modPow(Pp1d4, P); //get the root of ySquared
-//var y = P.minus(ySquared.modPow(Pp1d4, P)).mod(P); //i can also get the other answer if i wanted by subtracting the first result from P
+//var y = P.minus(ySquared.modPow(Pp1d4, P)).mod(P); 
 console.log("y                = " + y.toString(16));
 
-var ecPointR = [x, y];
-console.log("ecPointR         = " + ecPointR[0].toString(16) + ", " + ecPointR[1].toString(16));
-console.log("or");
-console.log("ecPointR         = " + ecPointR[0].toString(16) + ", " + P.minus(ecPointR[1]).mod(P).toString(16));
+console.log("-------------");
+console.log();
+console.log("2 ecPoints where x = R. I only need one of them, doesn't matter which");
+console.log();
+var ecPointK = [x, y];
+console.log("ecPointK (1)     = " + ecPointK[0].toString(16) + ", " + ecPointK[1].toString(16));
+console.log("ecPointK (2)     = " + ecPointK[0].toString(16) + ", " + P.minus(ecPointK[1]).mod(P).toString(16));
+
+//pubKey  = (ecPointK * (S/R)) - ecPoint(Z/R))
 
 console.log();
 
 
-//calculate some more constants
+console.log("-------------");
+console.log();
+console.log("calculate some more constants from the supplied R, S and Z values");
+console.log();
 
 var SdR = S.multiply(R.modInv(N)).mod(N);
 console.log("S/R              = " + SdR.toString(16));
@@ -168,8 +267,8 @@ console.log("S/R              = " + SdR.toString(16));
 var ZdR = Z.multiply(R.modInv(N)).mod(N);
 console.log("Z/R              = " + ZdR.toString(16));
 
-var ecPointRmSdR = mul(ecPointR, SdR);
-console.log("ecPointR * (S/R) = [" + ecPointRmSdR[0].toString(16) + ", " + ecPointRmSdR[1].toString(16) + "]");
+var ecPointKmSdR = mul(ecPointK, SdR);
+console.log("ecPointK * (S/R) = [" + ecPointKmSdR[0].toString(16) + ", " + ecPointKmSdR[1].toString(16) + "]");
 
 var ecPointZdR = mul([X, Y], ZdR);
 console.log("ecPoint(Z/R)     = [" + ecPointZdR[0].toString(16) + ", " + ecPointZdR[1].toString(16) + "]");
@@ -177,23 +276,19 @@ console.log("ecPoint(Z/R)     = [" + ecPointZdR[0].toString(16) + ", " + ecPoint
 console.log();
 console.log("-------------");
 console.log();
-console.log("4 Possible ecPoints for the public key");
+console.log("2 Possible ecPoints for the public key. (depends on which ecPointK was used)");
 console.log();
-var ecPointPubKey1 = sub(ecPointRmSdR, ecPointZdR);
-var ecPointPubKey2 = sub(ecPointZdR, ecPointRmSdR);
-var ecPointPubKey3 = sub(negate(ecPointRmSdR), ecPointZdR);
-var ecPointPubKey4 = sub(ecPointZdR, negate(ecPointRmSdR));
+var ecPointPubKey1 = sub(ecPointKmSdR, ecPointZdR); //pubKey  = ecPoint(K*(S/R)) - ecPoint(Z`   11111QA2/R)
+var ecPointPubKey2 = sub(negate(ecPointKmSdR), ecPointZdR); //when using P - root of ySquared value
 
 
-console.log("(1) pubKeyPoint  = [" + ecPointPubKey1[0].toString(16) + ", " + ecPointPubKey1[1].toString(16) + "]");
-console.log("(2) pubKeyPoint  = [" + ecPointPubKey2[0].toString(16) + ", " + ecPointPubKey2[1].toString(16) + "]");
-console.log("(3) pubKeyPoint  = [" + ecPointPubKey3[0].toString(16) + ", " + ecPointPubKey3[1].toString(16) + "]");
-console.log("(4) pubKeyPoint  = [" + ecPointPubKey4[0].toString(16) + ", " + ecPointPubKey4[1].toString(16) + "]");
+console.log("pubKeyPoint (1)  = [" + ecPointPubKey1[0].toString(16) + ", " + ecPointPubKey1[1].toString(16) + "]");
+console.log("pubKeyPoint (2)  = [" + ecPointPubKey2[0].toString(16) + ", " + ecPointPubKey2[1].toString(16) + "]");
 console.log();
 
 console.log("-------------");
 console.log();
-console.log("8 possible crypto formatted public keys");
+console.log("4 possible crypto formatted public keys. (depends on which ecPointK was used)");
 console.log();
 
 console.log("uncompressed pubkey (1)        = 04" + ecPointPubKey1[0].toString(16) + ecPointPubKey1[1].toString(16));
@@ -202,72 +297,13 @@ if(ecPointPubKey1[1].mod(TWO).eq(ZERO)){
 }else{
     console.log("compressed pubkey (1) (odd y)  = 03" + ecPointPubKey1[0].toString(16));
 }
-
 console.log();
-console.log("uncompressed pubkey (1)        = 04" + ecPointPubKey2[0].toString(16) + ecPointPubKey2[1].toString(16));
+
+console.log("uncompressed pubkey (2)        = 04" + ecPointPubKey2[0].toString(16) + ecPointPubKey2[1].toString(16));
 if(ecPointPubKey2[1].mod(TWO).eq(ZERO)){
-    console.log("compressed pubkey (1) (even y) = 02" + ecPointPubKey2[0].toString(16));
+    console.log("compressed pubkey (2) (even y) = 02" + ecPointPubKey2[0].toString(16));
 }else{
-    console.log("compressed pubkey (1) (odd y)  = 03" + ecPointPubKey2[0].toString(16));
-}
-
-console.log();
-console.log("uncompressed pubkey (1)        = 04" + ecPointPubKey3[0].toString(16) + ecPointPubKey3[1].toString(16));
-if(ecPointPubKey3[1].mod(TWO).eq(ZERO)){
-    console.log("compressed pubkey (1) (even y) = 02" + ecPointPubKey3[0].toString(16));
-}else{
-    console.log("compressed pubkey (1) (odd y)  = 03" + ecPointPubKey3[0].toString(16));
-}
-
-console.log();
-console.log("uncompressed pubkey (1)        = 04" + ecPointPubKey4[0].toString(16) + ecPointPubKey4[1].toString(16));
-if(ecPointPubKey4[1].mod(TWO).eq(ZERO)){
-    console.log("compressed pubkey (1) (even y) = 02" + ecPointPubKey4[0].toString(16));
-}else{
-    console.log("compressed pubkey (1) (odd y) = 03" + ecPointPubKey4[0].toString(16));
+    console.log("compressed pubkey (2) (odd y)  = 03" + ecPointPubKey2[0].toString(16));
 }
 console.log();
 
-
-
-//var oneG = mul([X, Y], ONE);
-//console.log("1  " + oneG[0].toString(16) + " " + oneG[1].toString(16));
-
-// var twoG = add(oneG, oneG);
-// console.log("2  " + twoG[0].toString(16) + " " + twoG[1].toString(16));
-
-// var minus1G = sub(oneG, twoG)
-// console.log("-1 " + minus1G[0].toString(16) + " " + minus1G[1].toString(16));
-
-// var halfTwoG = div(twoG, TWO)
-// console.log("1  " + halfTwoG[0].toString(16) + " " + halfTwoG[1].toString(16));
-
-// var twoG = mul([X, Y], TWO);
-// console.log(twoG[0].toString(16) + " " + twoG[1].toString(16));
-
-// var threeG = mul([X, Y], THREE);
-// console.log("3  " + threeG[0].toString(16) + " " + threeG[1].toString(16));
-
-
-// var fourG = mul([X, Y], new BN(4));
-// console.log(fourG[0].toString(16) + " " + fourG[1].toString(16));
-
-// var fiveG = mul([X, Y], new BN(5));
-// console.log(fiveG[0].toString(16) + " " + fiveG[1].toString(16));
-
-// var sixG = mul([X, Y], new BN(6));
-// console.log("6  " + sixG[0].toString(16) + " " + sixG[1].toString(16));
-
-// var half6G = div(sixG, TWO);
-// console.log("3  " + half6G[0].toString(16) + " " + half6G[1].toString(16));
-
-//var testG = div(oneG, TWO);
-//console.log("t  " + testG[0].toString(16) + " " + testG[1].toString(16));
-
-// var testG = [ZERO, ZERO];
-// console.log("0  " + testG[0].toString(16) + " " + testG[1].toString(16));
-// for (var i = 1; i < 10; i++) {
-//     testG = add(testG, oneG);
-//     console.log(i + "\t" + testG[0].toString(16) + " " + testG[1].toString(16));
-
-// }
