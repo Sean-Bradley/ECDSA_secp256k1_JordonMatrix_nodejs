b0ec6e9c3c9944ea7a6335a11455eada81b84022
sean bradley <seanwasere@gmail.com>
2019-05-20 17:39:27 +0100

    python 3 version of getpubkeyfromrsz created

--- getPubKeyFromRSZ.py
+++ getPubKeyFromRSZ.py
@@ -7,26 +7,23 @@ def jordan_isinf(p):
 
 
 def mulcoords(c1, c2):
-    return [c1[0] * c2[0] % P, c1[1] * c2[1] % P]
+    return (c1[0] * c2[0] % P, c1[1] * c2[1] % P)
 
 
 def mul_by_const(c, v):
-    return [c[0] * v % P, c[1]]
+    return (c[0] * v % P, c[1])
 
 
 def addcoords(c1, c2):
-    return [c1[0] * c2[1] + c2[0] * c1[1] % P, c1[1] * c2[1] % P]
+    return ((c1[0] * c2[1] + c2[0] * c1[1]) % P, c1[1] * c2[1] % P)
 
 
 def subcoords(c1, c2):
-    aa = c1[0] * c2[1] % P
-    bb = c2[0] * c1[1] % P
-    cc = (aa - bb) + P % P
-    return [cc % P, (c1[1] * c2[1]) % P]
+    return ((c1[0] * c2[1] - c2[0] * c1[1]) % P, c1[1] * c2[1] % P)
 
 
 def invcoords(c):
-    return [c[1], c[0]]
+    return (c[1], c[0])
 
 
 def jordan_add(a, b):
@@ -34,50 +31,50 @@ def jordan_add(a, b):
         return b
     if jordan_isinf(b):
         return a
-    if ((a[0][0] * b[0][1]) - (b[0][0] * a[0][1]) % P == 0):
-        if ((a[1][0] * b[1][1]) - (b[1][0] * a[1][1]) % P == 0):
+    if (a[0][0] * b[0][1] - b[0][0] * a[0][1]) % P == 0:
+        if (a[1][0] * b[1][1] - b[1][0] * a[1][1]) % P == 0:
             return jordan_double(a)
         else:
-            return [[0, 1], [0, 1]]
+            return ((0, 1), (0, 1))
 
     xdiff = subcoords(b[0], a[0])
     ydiff = subcoords(b[1], a[1])
     m = mulcoords(ydiff, invcoords(xdiff))
     x = subcoords(subcoords(mulcoords(m, m), a[0]), b[0])
     y = subcoords(mulcoords(m, subcoords(a[0], x)), a[1])
-    return [x, y]
+    return (x, y)
 
 
 def jordan_double(a):
     if jordan_isinf(a):
-        return [[0, 1], [0, 1]]
+        return ((0, 1), (0, 1))
     num = addcoords(mul_by_const(mulcoords(a[0], a[0]), 3), [0, 1])
     den = mul_by_const(a[1], 2)
     m = mulcoords(num, invcoords(den))
     x = subcoords(mulcoords(m, m), mul_by_const(a[0], 2))
     y = subcoords(mulcoords(m, subcoords(a[0], x)), a[1])
-    return [x, y]
+    return (x, y)
 
 
 def jordan_multiply(a, n):
     if jordan_isinf(a) or n == 0:
-        return ([0, 0], [0, 0])
-    if (n == 1):
+        return ((0, 0), (0, 0))
+    if n == 1:
         return a
-    if (n < 0 or n > N):
+    if n < 0 or n >= N:
         return jordan_multiply(a, n % N)
-    if (n % 2 == 0):
-        return jordan_double(jordan_multiply(a, n / 2))
-    if (n % 2 == 1):
-        return jordan_add(jordan_double(jordan_multiply(a, n / 2)), a)
+    if n % 2 == 0:
+        return jordan_double(jordan_multiply(a, n // 2))
+    if n % 2 == 1:
+        return jordan_add(jordan_double(jordan_multiply(a, n // 2)), a)
 
 
 def to_jordan(p):
-    return [[p[0], 1], [p[1], 1]]
+    return ((p[0], 1), (p[1], 1))
 
 
 def from_jordan(p):
-    return [p[0][0] * modInv(p[0][1], P) % P, p[1][0] * modInv(p[1][1], P) % P]
+    return (p[0][0] * modInv(p[0][1], P) % P, p[1][0] * modInv(p[1][1], P) % P)
 
 
 def mul(a, n):
@@ -113,16 +110,15 @@ def sub(a, b):
     @param {number} a - An ECPoint
     @param {number} b - An ECPoint
     """
-
-    return from_jordan(jordan_add(to_jordan(a), to_jordan([b[0], P - (b[1] % P)])))
+    return from_jordan(jordan_add(to_jordan(a), to_jordan((b[0], P - (b[1] % P)))))
 
 
 def negate(a):
-    return [a[0], P - (a[1] % P)]
+    return (a[0], P - (a[1] % P))
 
 
 def ecPoint(a):
-    return mul([X, Y], a)
+    return mul((X, Y), a)
 
 
 # Extract Bitcoin Public Key using R, S and Z values.
@@ -131,7 +127,6 @@ def ecPoint(a):
 # 2²⁵⁶ - 2³² - 2⁹ - 2⁸ - 2⁷ - 2⁶ - 2⁴ - 1
 P = 0xfffffffffffffffffffffffffffffffffffffffffffffffffffffffefffffc2f
 N = 0xfffffffffffffffffffffffffffffffebaaedce6af48a03bbfd25e8cd0364141
-SEVEN = 0x7
 X = 0x79be667ef9dcbbac55a06295ce870b07029bfcdb2dce28d959f2815b16f81798
 Y = 0x483ada7726a3c4655da4fbfc0e1108a8fd17b448a68554199c47d08ffb10d4b8
 
@@ -236,7 +231,7 @@ print
 print("2 ecPoints where x = R. I only need one of them, doesn't matter which")
 print
 
-ecPointK = [x, y]
+ecPointK = (x, y)
 print("ecPointK (1)     = 0x%x, 0x%x" % (ecPointK[0], ecPointK[1]))
 print("ecPointK (2)     = 0x%x, 0x%x" % (ecPointK[0], (P - (ecPointK[1] % P))))
 
@@ -260,12 +255,11 @@ print("Z/R              = 0x%x" % ZdR)
 
 ecPointKmSdR = mul(ecPointK, SdR)
 print("ecPointK * (S/R) = [0x%x,0x%x]" % (ecPointKmSdR[0], ecPointKmSdR[1]))
-exit()
 ecPointZdR = mul([X, Y], ZdR)
 print("ecPoint(Z/R)     = [0x%x,0x%x]" % (ecPointZdR[0], ecPointZdR[1]))
 
 print()
-print()("-------------")
+print("-------------")
 print()
 print("2 Possible ecPoints for the public key. (depends on which ecPointK was used)")
 print()
@@ -294,7 +288,7 @@ else:
 
 print()
 
-print("uncompressed pubkey (2)        = \n%x%x" %
+print("uncompressed pubkey (2)        = \n04%x%x" %
       (ecPointPubKey2[0], ecPointPubKey2[1]))
 if ecPointPubKey2[1] % 2 == 0:
     print("compressed pubkey (2) (even y) = \n02%x" % (ecPointPubKey2[0]))
@@ -304,14 +298,15 @@ else:
 print()
 
 
+# simple test
 #  1st i create an ecpoint of 10 (secp256k1)
-#myECPoint = mul([x, y], 10)
+myECPoint = mul((x, y), 10)
 # console.log("myECPoint = [" + myECPoint[0].toString(16) + \
 #             ", " + myECPoint[1].toString(16) + "]")
 # // next i will divide it by 5. must convert 5 to a bignumber
-# var ecResult = div(myECPoint, new BN(5))
+ecResult = div(myECPoint, 5)
 # console.log("ecResult = [" + ecResult[0].toString(16) + \
 #             ", " + ecResult[1].toString(16) + "]")
 # // //result should equal ecpoint of 2 (secp256k1)
-# var ecTest = mul([x, y], new BN(2)) // expected result
-# console.log(ecResult[0].toString(16) === ecTest[0].toString(16) & & ecResult[1].toString(16) == = ecTest[1].toString(16))
+ecTest = mul((x, y), 2)  # expected result
+print(ecResult[0] == ecTest[0] and ecResult[1] == ecTest[1])
